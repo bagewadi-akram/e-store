404acfdb9451c87c05f512cd70bbeb4dd26cd071
Akram Bagewadi <akram.webdevelopr@gmail.com>
2022-08-18 15:27:56 +0530

    header added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmK1Ac_yh7SKk4UAdRdMwLLnIty6IlJpo",
+  authDomain: "e-store-f8f5b.firebaseapp.com",
+  projectId: "e-store-f8f5b",
+  storageBucket: "e-store-f8f5b.appspot.com",
+  messagingSenderId: "760456621304",
+  appId: "1:760456621304:web:e158350be05f35a3c2df0d",
+  measurementId: "G-LP8X5Z4Z21",
+};
