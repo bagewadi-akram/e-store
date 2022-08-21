77dba7dc803f35930b681a9df1cb9a54443863c7
Akram Bagewadi <akram.webdevelopr@gmail.com>
2022-08-21 18:53:14 +0530

    intial commit

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{🧺 }, dispatch] = useStateValue;
   return (
     <div className="header">
       <Link to="/">
@@ -33,7 +35,7 @@ function Header() {
         <Link to="/checkout">
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header_basketCount">0</span>
+            <span className="header__optionLineTwo header_basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
